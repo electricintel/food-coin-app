1823b7bf09f82af17568129fa2fc252b8a5f8212
Muhammad Dadu <dadum01@muhammads-mbp.home>
2017-10-31 23:10:45 +0000

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Diagnostic } from '@ionic-native/diagnostic';
+import { QRScanner } from '@ionic-native/qr-scanner';
+import { Vibration } from '@ionic-native/vibration';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -24,6 +27,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    Diagnostic,
+    QRScanner,
+    Vibration,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,77 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ToastController } from 'ionic-angular';
+import { Diagnostic } from '@ionic-native/diagnostic';
+import { QRScanner } from '@ionic-native/qr-scanner';
+import { Vibration } from '@ionic-native/vibration';
+
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	scanSubscription: any = null;
+	account = {
+		qrCode: 'dsa',
+		password: '',
+	};
 
-  }
+	constructor(
+		public navCtrl: NavController,
+        public toastCtrl: ToastController,
+		private diagnostic: Diagnostic,
+		private qrScanner: QRScanner,
+		private vibration: Vibration
+	) {
+	}
 
+	ionViewDidLoad() {
+	    this.checkCameraPermissions()
+	    	.then(() => this.initializeQRCameraPreview())
+	    	.catch((errorMessage) => {
+				this.toastCtrl.create({
+				    message: errorMessage, 
+				    position: 'bottom',
+				    duration: 5000
+				}).present()
+	    	});
+	}
+
+	ionViewWillLeave() {
+		if (this.scanSubscription) {
+			this.qrScanner.hide();
+			this.scanSubscription.unsubscribe();
+			this.scanSubscription = null;
+		}
+	}
+	 
+	checkCameraPermissions(): Promise<any> {
+		return this.diagnostic.isCameraAuthorized().then((authorized) => {
+		    if (authorized) {
+		        return;
+		    }
+
+	        return this.diagnostic.requestCameraAuthorization().then((status) => {
+	            if (status !== this.diagnostic.permissionStatus.GRANTED) {
+	                return Promise.reject('Cannot access camera');
+	            }
+	        });
+        });
+	}
+
+	initializeQRCameraPreview() {
+		this.scanSubscription = this.qrScanner.scan().subscribe((text: string) => {
+			this.account.qrCode = text;
+			this.vibration.vibrate(1000);
+
+			this.qrScanner.pausePreview();
+			this.scanSubscription.unsubscribe();
+			this.scanSubscription = null;
+		});
+
+		// show camera preview
+		this.qrScanner.show();
+	}
 }
